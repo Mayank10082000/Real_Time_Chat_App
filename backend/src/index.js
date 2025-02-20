a29771bd655827ba87c2d88d057f8250a4fdc61e
Mayank Gupta <mayank.gupta8@stryker.com>
2025-02-20 19:57:27 +0530

    added many functionalities

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); // Middleware to extract data from the json body
+app.use(cookieParser()); // Allow us to parse the cookies and get the values out of it
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:", PORT);
