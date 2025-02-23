15100f2f8cf7eaecedbe31707664dee68805939a
Mayank Gupta <mayank.gupta8@stryker.com>
2025-02-23 16:00:57 +0530

    ui components added

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -18,7 +18,7 @@ export const protectRoute = async (req, res, next) => {
       return res.status(401).json({ message: "Unauthorized - Invalid token" });
     }
 
-    const user = await User.findById(decoded.userId).select("-password");
+    const user = await User.findById(decoded.userId).select("-password"); //Select all data except password
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
