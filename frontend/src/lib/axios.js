aee5b4fdb24caeb5ffe64018402a5f1f2ac42904
Mayank Gupta <mayank.gupta8@stryker.com>
2025-02-26 15:55:03 +0530

    fixed a bug

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "developement"
+    import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
       : "/api",
   withCredentials: true, // Used to send cookies with every single request
